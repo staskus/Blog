4e848848c5bc63ec4ea2013838ffbf6c559ccfda
Povilas Staskus <povilas.staskus@gmail.com>
2020-01-26 10:49:40 +0200

    Added PureCSS Grid

---
+++ Sources/PureCSS/Node+Grid.swift
@@ -0,0 +1,18 @@
+//
+//  Node+Grid.swift
+//  
+//
+//  Created by Povilas Staskus on 1/26/20.
+//
+
+import Plot
+
+private extension Node where Context == HTML.BodyContext {
+    static func grid(_ nodes: Node...) -> Node {
+        .div(
+            .id("layout"),
+            .class("pure-g"),
+            .group(nodes)
+        )
+    }
+}
