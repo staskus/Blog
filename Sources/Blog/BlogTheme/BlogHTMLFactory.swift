45e5de75381cf86ad103ff4da061af4700702f5f
Povilas Staskus <povilas.staskus@gmail.com>
2020-01-26 10:46:18 +0200

    Created Head

--- Sources/Blog/BlogTheme/BlogHTMLFactory.swift
+++ Sources/Blog/BlogTheme/BlogHTMLFactory.swift
@@ -11,37 +11,43 @@ import Plot
 struct BlogHTMLFactory: HTMLFactory {
     func makeIndexHTML(for index: Index, context: PublishingContext<Blog>) throws -> HTML {
         HTML(
-            .lang(context.site.language)
+            .lang(context.site.language),
+            .head(for: context.site)
         )
     }
     
     func makeSectionHTML(for section: Section<Blog>, context: PublishingContext<Blog>) throws -> HTML {
         HTML(
-            .lang(context.site.language)
+            .lang(context.site.language),
+            .head(for: context.site)
         )
     }
     
     func makeItemHTML(for item: Item<Blog>, context: PublishingContext<Blog>) throws -> HTML {
         HTML(
-            .lang(context.site.language)
+            .lang(context.site.language),
+            .head(for: context.site)
         )
     }
     
     func makePageHTML(for page: Page, context: PublishingContext<Blog>) throws -> HTML {
         HTML(
-            .lang(context.site.language)
+            .lang(context.site.language),
+            .head(for: context.site)
         )
     }
     
     func makeTagListHTML(for page: TagListPage, context: PublishingContext<Blog>) throws -> HTML? {
         HTML(
-            .lang(context.site.language)
+            .lang(context.site.language),
+            .head(for: context.site)
         )
     }
     
     func makeTagDetailsHTML(for page: TagDetailsPage, context: PublishingContext<Blog>) throws -> HTML? {
         HTML(
-            .lang(context.site.language)
+            .lang(context.site.language),
+            .head(for: context.site)
         )
     }
 }
---
+++ Sources/Blog/BlogTheme/Head.swift
@@ -0,0 +1,39 @@
+//
+//  BlogHead.swift
+//  
+//
+//  Created by Povilas Staskus on 1/26/20.
+//
+
+import Publish
+import Plot
+
+extension Node where Context == HTML.DocumentContext {
+    static func head(for site: Blog) -> Node {
+        return Node.head(
+            .meta(
+                .charset(.utf8),
+                .name("viewport"),
+                .content("width=device-width, initial-scale=1")
+            ),
+            .link(
+                .rel(.stylesheet),
+                .href("https://unpkg.com/purecss@1.0.1/build/pure-min.css"),
+                .init(name: "integrity", value: "sha384-oAOxQR6DkCoMliIh8yFnu25d7Eq/PHS21PClpwjOTeU2jRSq11vu66rf90/cZr47"),
+                .init(name: "crossorigin", value: "anonymous")
+            ),
+            .link(
+                .rel(.stylesheet),
+                .href("https://unpkg.com/purecss@1.0.1/build/grids-responsive-min.css")
+            ),
+            .link(
+                .rel(.stylesheet),
+                .href("/Pure/styles.css")
+            ),
+            .link(
+                .rel(.stylesheet),
+                .href("/all.css")
+            )
+        )
+    }
+}
