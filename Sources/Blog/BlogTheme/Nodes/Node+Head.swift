f143506378d15a493577eca3e31b8963cc1f2844
Povilas Staskus <povilas.staskus@revelsystems.com>
2020-05-11 17:41:50 +0300

    Added title

--- Sources/Blog/BlogTheme/Nodes/Node+Head.swift
+++ Sources/Blog/BlogTheme/Nodes/Node+Head.swift
@@ -10,6 +10,7 @@ import Plot
 extension Node where Context == HTML.DocumentContext {
     static func head(for site: Blog) -> Node {
         return Node.head(
+            .title("\(site.name) - \(site.description)"),
             .meta(
                 .charset(.utf8),
                 .name("viewport"),
